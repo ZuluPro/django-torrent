2e22da67b8a519047622f710748d4bdcbec44adb
ZuluPro <montheanthony@hotmail.com>
2017-08-02 19:51:37 +0200

    Added metas

---
+++ dj_torrent/__init__.py
@@ -0,0 +1,8 @@
+"""
+Automatically search and download subscribed TV shows and magazines using
+torrentz.eu, myepisodes.com and transmissionrpc
+"""
+VERSION = (0, 0, 1)
+__version__ = '.'.join([str(i) for i in VERSION])
+__author__ = 'Takeshi Kanemoto (tkanemoto)'
+__url__ = 'https://github.com/tkanemoto/django-torrent'
